99f4688f08388383c817b6b45bb71722ae16858f
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-10-28 17:55:20 +0900

    grass 별 y 좌표 다르게 하는거 겨우 성공

--- grass.py
+++ grass.py
@@ -1,13 +1,15 @@
 from pico2d import load_image
 
 class Grass():
+    next_y = 20
+
     def __init__(self):
         self.image = load_image('grass.png')
+        self.y = Grass.next_y
+        Grass.next_y -= 20
 
     def draw(self):
-        grass_y = 20
-        self.image.draw(400, grass_y)
-        grass_y -= 20
+        self.image.draw(400, self.y)
 
     def update(self):
         pass
