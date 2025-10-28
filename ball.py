f04de454526d63baefa6f7285f6be0118cdc5c3b
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-10-28 17:20:53 +0900

    2022180040 최민우 과제 시작

---
+++ ball.py
@@ -0,0 +1,24 @@
+from pico2d import load_image
+
+import game_world
+
+
+class Ball:
+
+    image = None
+
+    def __init__(self, x = 400 , y = 400, velocity = 1):
+        if Ball.image is None:
+            Ball.image = load_image('ball21x21.png')
+        self.x = x
+        self.y = y
+        self.velocity = velocity
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        self.x += self.velocity
+        #화면 밖으로 나가면 제거
+        if self.x < 0 or self.x > 800:
+            game_world.remove_object(self)
